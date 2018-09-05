6fb91fa8a09a07a6de6f7e5457965bf7217c45e4
Liam Giuliano-Kuhn <liam.gkuhn@gmail.com>
2018-09-05 13:47:18 +1000

    New Additions

--- attempt2.rb
+++ attempt2.rb
@@ -1,44 +1,55 @@
 require_relative "moveclass.rb"
 require_relative "textclass.rb"
 
-
+#Initializing the classes and variables
 text = Text.new()
-text.welcomeText()
-
+playerMove = Move.new()
 position = 50
 ballPossession = true
 gamePlaying = true
+moveCount = 10
+goals = 0
 
-playerMove = Move.new()
+#Welcome Screen
+text.welcomeText()
 text.gameStartMoveText()
 
+#Defining the game loop and the exit conditon to the final victory/loss screen
+while gamePlaying == true do
 
-while position >= 0 && position <= 100 do
-    newposition = 0
-    newposition = newposition + position
+    #Defining where gameplay switches from offense to defense
+    if ballPossession == true
     
-    userInput = gets.chomp.to_i
-    if userInput == 1
-        playerMove.run(newposition)
-
-    elsif userInput == 2
-        playerMove.dodge(newposition)
+        while position > 0 && position < 100 do
+        
+        
+        #Offensive Move Selection
+        userInput = gets.chomp.to_i
+        if userInput == 1
+            
+            puts "Your new position is #{position += playerMove.run(0)}"
+        
+        elsif userInput == 2
+            puts "Your new positions is #{position += playerMove.dodge(0)}"
+        end
+          
     end
-
+    #This is where the defense moves go
     
-
 end
-
+#Win and loss Conditions
 if position >= 100
+    gamePlaying = false
     text.victoryText()
 
 else 
     text.lossText()
+    gamePlaying = false
 
 end
 
 
-
+end
 
 
 #making sure it works still
--- moveclass.rb
+++ moveclass.rb
@@ -6,22 +6,26 @@ class Move
     def run(position)
         if ((rand() + 0.2) > 0.5)
         position += 10
-        test = true
+        puts "Move Succesful"
+        
         
         else  
+            position -= 10
             puts "Move Failed"
         end
-        puts position
-        puts test
+        return position
+        
     end
 
     def dodge(position)
         if ((rand() + 0.5) > 0.5)
             position += 5
-            puts position
+            puts "Move Successful"
         else
+            position -= 5
             puts "Move Failed"
         end
+        return position
     end
 
     def test()
